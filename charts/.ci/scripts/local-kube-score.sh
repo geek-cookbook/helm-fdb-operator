d628838476b3729bc750bb3b4ad169098fc1f8ff
David Young <davidy@funkypenguin.co.nz>
2020-12-10 14:22:38 +1300

    Make chart pass kube-score

--- charts/.ci/scripts/local-kube-score.sh
+++ charts/.ci/scripts/local-kube-score.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-helm template charts/* -f charts/.ci/values-kube-score.yaml | kube-score score -
+helm template --values charts/.ci/values-kube-score.yaml charts/* -f charts/.ci/values-kube-score.yaml | kube-score score - \
+    --ignore-test pod-networkpolicy \
+    --ignore-test deployment-has-poddisruptionbudget \
+    --ignore-test deployment-has-host-podantiaffinity
